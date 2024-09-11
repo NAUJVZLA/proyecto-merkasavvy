4a98405ad8bb64a8f1894b862bde3435e86209f5
Kevin Rodas Cuartas <kevin.rodas.cuartas@gmail.com>
2024-09-11 08:52:58 -0500

    change icons and views in home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,139 +6,44 @@ import Imagen from "@/components/types/svg/image"; // para zoom pendiente por im
 export default function Home() {
   const list = [
     {
-      title: "Orange",
+      title: "Create List",
       img: "https://nextui.org/images/fruit-1.jpeg",
       price: "$5.50",
     },
     {
-      title: "Tangerine",
+      title: "My Lists",
       img: "https://nextui.org/images/fruit-2.jpeg",
       price: "$3.00",
     },
     {
-      title: "Raspberry",
+      title: "Budget",
       img: "https://nextui.org/images/fruit-3.jpeg",
       price: "$10.00",
     },
     {
-      title: "Lemon",
+      title: "Information",
       img: "https://nextui.org/images/fruit-4.jpeg",
       price: "$5.30",
-    },
-    {
-      title: "Avocado",
-      img: "https://nextui.org/images/fruit-5.jpeg",
-      price: "$15.70",
-    },
-    {
-      title: "Lemon 2 ",
-      img: "https://nextui.org/images/fruit-6.jpeg",
-      price: "$8.00",
-    },
-    {
-      title: "Banana",
-      img: "https://nextui.org/images/fruit-7.jpeg",
-      price: "$7.50",
-    },
-    {
-      title: "Watermelon",
-      img: "https://nextui.org/images/fruit-8.jpeg",
-      price: "$12.20",
-    },
-    {
-      title: "Orange",
-      img: "https://nextui.org/images/fruit-1.jpeg",
-      price: "$5.50",
-    },
-    {
-      title: "Tangerine",
-      img: "https://nextui.org/images/fruit-2.jpeg",
-      price: "$3.00",
-    },
-    {
-      title: "Raspberry",
-      img: "https://nextui.org/images/fruit-3.jpeg",
-      price: "$10.00",
-    },
-    {
-      title: "Lemon",
-      img: "https://nextui.org/images/fruit-4.jpeg",
-      price: "$5.30",
-    },
-    {
-      title: "Avocado",
-      img: "https://nextui.org/images/fruit-5.jpeg",
-      price: "$15.70",
-    },
-    {
-      title: "Lemon 2",
-      img: "https://nextui.org/images/fruit-6.jpeg",
-      price: "$8.00",
-    },
-    {
-      title: "Banana",
-      img: "https://nextui.org/images/fruit-7.jpeg",
-      price: "$7.50",
-    },
-    {
-      title: "Watermelon",
-      img: "https://nextui.org/images/fruit-8.jpeg",
-      price: "$12.20",
-    },
-    {
-      title: "Orange",
-      img: "https://nextui.org/images/fruit-1.jpeg",
-      price: "$5.50",
-    },
-    {
-      title: "Tangerine",
-      img: "https://nextui.org/images/fruit-2.jpeg",
-      price: "$3.00",
-    },
-    {
-      title: "Raspberry",
-      img: "https://nextui.org/images/fruit-3.jpeg",
-      price: "$10.00",
-    },
-    {
-      title: "Lemon",
-      img: "https://nextui.org/images/fruit-4.jpeg",
-      price: "$5.30",
-    },
-    {
-      title: "Avocado",
-      img: "https://nextui.org/images/fruit-5.jpeg",
-      price: "$15.70",
-    },
-    {
-      title: "Lemon 2",
-      img: "https://nextui.org/images/fruit-6.jpeg",
-      price: "$8.00",
-    },
-    {
-      title: "Banana",
-      img: "https://nextui.org/images/fruit-7.jpeg",
-      price: "$7.50",
     }
   ];
 
   return (
-    <div className="gap-2 grid grid-cols-2 sm:grid-cols-3">
+    <div className="gap-2 grid grid-cols-2 sm:grid-cols-2">
       {list.map((item, index) => (
         <Card
           shadow="sm"
           key={index}
           isPressable
-          onPress={() => console.log("item pressed")}
         >
           <CardBody className="overflow-visible p-0">
             <Image
               shadow="sm"
               radius="lg"
               width="100%"
+              height="50vh"
               alt={item.title}
               className="w-full object-cover h-[140px]"
-              src={item.img}
+              src="/assets/BUDGET.jpeg"
             />
           </CardBody>
           <CardFooter className="text-small justify-between">
