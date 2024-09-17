8924302f7cf265fc8d694a87cc3e0feb40c906bf
Kevin Rodas Cuartas <kevin.rodas.cuartas@gmail.com>
2024-09-17 08:34:56 -0500

    create logic new array for use in the other component

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,20 +1,28 @@
 "use client";
 import React from "react";
+import { useEffect } from "react";
 import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
-import {useRouter} from "next/navigation"
+import {useRouter} from "next/navigation";
+import Link from "next/link";
 import Nav from "../navGeneral/page"
+
+
+
 export default function Home() {
-  const router = useRouter();
-
-  const handleCreateList = () =>{
-    router.push('/products-dashboard')
+  //const router = useRouter();
+  const token = sessionStorage.getItem('token')
+  useEffect(() => {
+     if(!token){
+    alert("no estas autenticado, debes iniciar sesión")
+    router.push('/login')
   }
-
-  const auth = sessionStorage.getItem('token')
+  }, []);
+ 
+  
   return (
     <>
     <Nav/>
-    <h1><b>Bienvenido {auth}</b></h1>
+    <h1><b>Bienvenido {token}</b></h1>
     <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
         <Card
           shadow="sm"
@@ -32,7 +40,9 @@ export default function Home() {
           </CardBody>
           <CardFooter className="text-small justify-between">
             <b>Create List</b>
-            <button className="text-default-500" onClick={handleCreateList}>Go <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg></button>
+            <Link href='/products-dashboard'>
+            <button className="text-default-500">Go <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg></button>
+            </Link>            
           </CardFooter>
         </Card>
         <Card
@@ -51,7 +61,9 @@ export default function Home() {
           </CardBody>
           <CardFooter className="text-small justify-between">
             <b>Purchase List</b>
+            <Link href='/purchase-list'>
             <button className="text-default-500">Go <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg></button>
+            </Link>            
           </CardFooter>
         </Card>
         <Card
@@ -70,7 +82,9 @@ export default function Home() {
           </CardBody>
           <CardFooter className="text-small justify-between">
             <b>Budget</b>
+            <Link href='/budget'>
             <button className="text-default-500">Go <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg></button>
+            </Link>            
           </CardFooter>
         </Card>
         <Card
@@ -88,8 +102,10 @@ export default function Home() {
             />
           </CardBody>
           <CardFooter className="text-small justify-between">
+            <Link href='/information'>
             <b>Information</b>
             <button className="text-default-500">Go <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg></button>
+            </Link>           
           </CardFooter>
         </Card>      
     </div>
